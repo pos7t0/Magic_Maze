39d3b4b3fc8b95612312af268e4a21cb523c50ee
vicente-santander <127953756+vicente-santander@users.noreply.github.com>
2024-11-25 20:19:04 -0300

    mini error en la clase arreglado

--- magic_maze/lib/models/magic_card.dart
+++ magic_maze/lib/models/magic_card.dart
@@ -1,11 +1,11 @@
 class MagicCard {
-  final int id;
+  final String id;
   final String name;
   final String type;
   final String rarity;
   final String text;
   final String manaCost;
-  final String colors;
+  final List<String> colors; // Cambiado a List<String>
   final int power;
   final int toughness;
   final String imageUrl;
@@ -26,15 +26,15 @@ class MagicCard {
   // Método para convertir la respuesta JSON en un objeto MagicCard
   factory MagicCard.fromJson(Map<String, dynamic> json) {
     return MagicCard(
-      id: json["id"],
-      name: json['name'],
-      type: json['type'],
-      rarity: json['rarity'],
-      text: json['text'],
-      manaCost: json["manaCost"],
-      colors: json["colors"],
-      power: json["power"],
-      toughness: json["toughness"],
+      id: json["id"] ?? '',
+      name: json['name'] ?? '',
+      type: json['type'] ?? '',
+      rarity: json['rarity'] ?? '',
+      text: json['text'] ?? '',
+      manaCost: json["manaCost"] ?? '',
+      colors: List<String>.from(json["colors"] ?? []), // Manejo como lista
+      power: int.tryParse(json["power"] ?? '0') ?? 0,
+      toughness: int.tryParse(json["toughness"] ?? '0') ?? 0,
       imageUrl: json['imageUrl'] ?? '',
     );
   }
