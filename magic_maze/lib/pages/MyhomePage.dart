c4b5e76274ee1523c15b08f67fca830f48441010
Rodrigo <rodrigo.osvaldo.2003@gmail.com>
2024-11-25 21:02:19 -0300

    Pasar informacion a otra pagina

--- magic_maze/lib/pages/MyhomePage.dart
+++ magic_maze/lib/pages/MyhomePage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:magic_maze/utils/api_helper.dart';
-import 'package:magic_maze/models/magic_card.dart';
+import 'package:magic_maze/pages/randomDeckPage.dart';
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -13,13 +13,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
-  final MagicApiHelper apiHelper = MagicApiHelper();
-  List<MagicCard> _cards = []; // Lista para almacenar las cartas obtenidas
 
   // Lista de las páginas que quieres mostrar
   static const List<Widget> _pages = <Widget>[
-    Center(child: Text('Página 1')),
-    Center(child: Text('Página 2')),
+    RandomDeckPage(),
+    RandomDeckPage(),
   ];
 
   void _onItemTapped(int index) {
@@ -28,57 +26,11 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // Método que se ejecuta al presionar el botón para obtener cartas aleatorias
-  void _fetchRandomCards() async {
-    try {
-      // Solicitar un número aleatorio de cartas dentro del rango permitido
-      const int randomCount = 60; // Cambiar a cualquier valor entre 60 y 100
-      List<MagicCard> cards =
-          await apiHelper.fetchRandomCards(count: randomCount);
-      setState(() {
-        _cards = cards;
-      });
-    } catch (e) {
-      // Si ocurre un error, mostramos un mensaje
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error al cargar cartas: $e')),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: _pages[_selectedIndex], // Página seleccionada
-            ),
-            ElevatedButton(
-              onPressed:
-                  _fetchRandomCards, // Llamar al método cuando se presiona el botón
-              child: const Text('Obtener Cartas'),
-            ),
-            // Mostrar las cartas obtenidas (si las hay)
-            if (_cards.isNotEmpty)
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _cards.length,
-                  itemBuilder: (context, index) {
-                    var card = _cards[index];
-                    return ListTile(
-                      title: Text(
-                          card.name), // Acceder a las propiedades directamente
-                      subtitle: Text(
-                          card.type), // Acceder a las propiedades directamente
-                    );
-                  },
-                ),
-              ),
-          ],
-        ),
+        child: _pages[_selectedIndex], // Mostrar la página seleccionada
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.shifting,
---
+++ magic_maze/lib/pages/randomDeckPage.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:magic_maze/models/magic_card.dart';
+import 'package:magic_maze/utils/api_helper.dart';
+
+class RandomDeckPage extends StatefulWidget {
+  const RandomDeckPage({Key? key}) : super(key: key);
+
+  @override
+  State<RandomDeckPage> createState() => _RandomDeckPageState();
+}
+
+class _RandomDeckPageState extends State<RandomDeckPage> {
+  final MagicApiHelper apiHelper = MagicApiHelper();
+  List<MagicCard> _cards = [];
+
+  // Método para obtener cartas aleatorias
+  void _fetchRandomCards() async {
+    try {
+      const int randomCount = 60; // Número de cartas a obtener
+      List<MagicCard> cards =
+          await apiHelper.fetchRandomCards(count: randomCount);
+      setState(() {
+        _cards = cards;
+      });
+    } catch (e) {
+      // Mostrar un mensaje en caso de error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al cargar cartas: $e')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Cartas'),
+      ),
+      body: Stack(
+        children: [
+          // Lista de cartas
+          if (_cards.isNotEmpty)
+            ListView.builder(
+              itemCount: _cards.length,
+              padding: const EdgeInsets.only(bottom: 80),
+              itemBuilder: (context, index) {
+                var card = _cards[index];
+                return ListTile(
+                  title: Text(card.name),
+                  subtitle: Text(card.type),
+                );
+              },
+            ),
+          // Botón en la parte inferior central
+          
+          Positioned(
+            bottom: 20,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: ElevatedButton(
+                onPressed: _fetchRandomCards,
+                child: const Text('Obtener Cartas'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
